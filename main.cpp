d5f118883c19119a3c040174e83669dac6c387fa
Hristina Todorova <todorovah@vmware.com>
2018-06-29 18:49:54 +0300

    add main.cpp - tic tac toe written in bad style

---
+++ main.cpp
@@ -0,0 +1,229 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+const int dim=3;
+int inf=20000;
+int ninf=-20000;
+char human;
+char computer;
+bool AIturn=true;
+char  board[dim][dim];
+
+void gen()
+{
+    for (int i=0;i<dim;i++)
+    {
+        for(int j=0;j<dim;j++)
+        {
+            board[i][j]=' ';
+        }
+    }
+}
+
+
+
+void print()
+{
+    for (int i=0;i<dim;i++)
+    {
+
+        for(int j=0;j<dim;j++)
+        {
+            cout<<'|'<<board[i][j];
+        }
+        cout<<'|'<<endl;
+    }
+}
+
+bool win(char ch)
+{
+    for(int i = 0; i < dim; i++) {
+        int n=dim;
+        while(--n>0 && board[i][n]==board[i][0]);
+        if(board[i][0] == ch && n==0) return true;
+        n=dim;
+
+
+        while(--n>0 && board[n][i]==board[0][i]);
+        if(board[0][i] == ch && n==0) return true;
+    }
+    int br=0;
+
+
+    while(board[br][br]==board[dim-1][dim-1])
+    {
+        br++;
+    }
+    if (board[0][0] == ch && br==dim) {
+        return true;
+    }
+
+
+    br =0;
+    int help=0;
+    while(board[help][dim-help-1]==board[0][dim-1])
+    {
+        br++;
+        help++;
+    }
+    if (board[0][dim-1] == ch && br==dim) {
+        return true;}
+
+    return false;
+
+}
+
+bool notempty()
+{
+    bool flag = true;
+    for (int i=0;i<dim;i++)
+    {
+        for(int j=0;j<dim;j++)
+        {
+            if(board[i][j]==' ')
+            {
+                return false;
+            }
+        }
+    }
+    return flag;
+}
+
+int score()
+{
+    if(win(computer))
+        return 1;
+    if(win(human))
+        return -1;
+    return 0;
+}
+
+ vector<pair<int,int> > allmoves()
+{
+    vector<pair<int,int> >temp;
+    if(notempty())
+    {
+        return temp;
+    }
+    for (int i=0;i<dim;i++)
+    {
+        for(int j=0;j<dim;j++)
+        {
+            if(board[i][j]==' ')
+            {pair<int,int> tmp(i,j);
+                temp.push_back(tmp);
+            }
+        }
+    }
+}
+
+vector<int> minmax(bool who,int alpha,int beta)
+{
+    vector<pair<int,int> > allposiblemoves;
+    allposiblemoves=allmoves();
+    int scoree;
+    int first=-1;
+    int second=-1;
+    if(notempty() || win(human) || win(computer))
+    {
+        scoree=score();
+        vector<int> temp;
+        temp.push_back(scoree);
+        temp.push_back(first);
+        temp.push_back(second);
+        return temp;    }
+    else{
+         for(int i=0;i<allposiblemoves.size();i++){
+             if(who==true)
+            {
+                 board[allposiblemoves[i].first][allposiblemoves[i].second]=computer;
+                 scoree=minmax(false,alpha,beta)[0];
+                if(scoree>alpha)
+                {
+                    alpha=scoree;
+                    first=allposiblemoves[i].first;
+                    second=allposiblemoves[i].second;
+                }
+            }
+             else{
+                 board[allposiblemoves[i].first][allposiblemoves[i].second]=human;
+                 scoree=minmax(true,alpha,beta)[0];
+                 if(scoree<beta)
+                {
+                    beta=scoree;
+                    first=allposiblemoves[i].first;
+                    second=allposiblemoves[i].second;
+                }
+            }
+             board[allposiblemoves[i].first][allposiblemoves[i].second]=' ';
+             if (alpha >= beta) break;}
+        if(who==true)
+        {
+            vector<int> temp;
+            temp.push_back(alpha);
+            temp.push_back(first);
+            temp.push_back(second);
+            return temp;
+
+
+        }
+        else{
+            vector<int> temp;
+            temp.push_back(beta);
+            temp.push_back(first);
+            temp.push_back(second);
+            return temp;
+
+
+
+        }
+    }
+
+}
+ int main()
+{
+    gen();
+    cout<<"choce x for second and o for first";
+    char a;
+    cin>>a;
+    human=a;
+    if(a=='o')
+    {
+        computer='x';
+
+    }
+    else{
+        computer='o';
+    }
+    int i,j;
+    print();
+    cout<<"turns start from 0 and end on dimention-1"<<endl;
+    while(!notempty() && !win(computer) && !win(human) )
+    {
+         if(human=='o')
+        {
+            cin>>i>>j;
+            board[i][j]='o';
+            print();
+            vector<int>temp=minmax(true,ninf,inf);
+             board[temp[1]][temp[2]]=computer;
+            print();
+        }
+         else{
+            vector<int>temp=minmax(true,ninf,inf);
+             board[temp[1]][temp[2]]=computer;
+            print();
+            if(win(human)||win(computer)||notempty())
+            {
+                break;
+            }
+            cin>>i>>j;
+            board[i][j]='x';
+            print();
+
+        }
+    }
+
+    return 0;
+}
